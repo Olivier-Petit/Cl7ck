1b4bad616bc1f5b2796f95f7c0e5d2518eae4363
op414 <petit-olivier@hotmail.fr>
2013-05-11 14:34:01 +0200

    Added information header to Cl7ck.ino

--- Cl7ck.ino
+++ Cl7ck.ino
@@ -1,3 +1,13 @@
+/*
+ * Cl7ck
+ * Author : op414
+ * http://op414.net
+ * License : CC BY-NC-SA http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * ------------
+ * Cl7ck.ino : Main file
+ * Created 11 may 2012
+ */
+ 
 void setup()
 {
 }
