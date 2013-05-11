56a0787495b9f7ae8b73f3c5947187b7d24af07a
op414 <lestyle414@hotmail.fr>
2013-05-11 18:06:22 +0200

    Add settings.h : pin definitions

---
+++ settings.h
@@ -0,0 +1,22 @@
+/*
+ * Cl7ck
+ * Author : op414
+ * http://op414.net
+ * License : CC BY-NC-SA http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * ------------
+ * settings.h : Various settings, mostly pin definitions
+ * Created 11 may 2012
+ */
+
+#indef DEF_SETTINGS
+#define DEF_SETTINGS
+
+#define PIN_BUZZER 3
+#define PIN_DISP   10
+#define PIN_PLUS   8
+#define PIN_MINUS  6
+#define PIN_BACK   5
+#define PIN_OK     7
+
+
+#endif
