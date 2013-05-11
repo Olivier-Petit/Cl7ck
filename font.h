35dd6cbcba58473fb5172148c044acba0d9927b4
op414 <lestyle414@hotmail.fr>
2013-05-11 16:37:05 +0200

    Add 7-segment alphanumeric font

---
+++ font.h
@@ -0,0 +1,103 @@
+/*
+ * Cl7ck
+ * Author : op414
+ * http://op414.net
+ * License : CC BY-NC-SA http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * ------------
+ * font.h : A 7-segment font file in ABCDEFG format (G is the lower bit, A the higher one)
+ * Created 11 may 2012
+ */
+ 
+#ifndef DEF_FONT
+#define DEF_FONT
+
+#define f_0 0x3F
+#define f_1 0x06
+#define f_2 0x5B
+#define f_3 0x4F
+#define f_4 0x66
+#define f_5 0x6D
+#define f_6 0x7D
+#define f_7 0x07
+#define f_8 0x7F
+#define f_9 0x6F
+
+#define f_A 0x77
+#define f_a 0x77
+
+#define f_B 0x7C
+#define f_b 0x7C
+
+#define f_C 0x39
+#define f_c 0x58
+
+#define f_D 0x5e
+#define f_d 0x5e
+
+#define f_E 0x79
+#define f_e 0x7B
+
+#define f_F 0x71
+#define f_f 0x71
+
+#define f_G 0x7D
+#define f_g 0x7D
+
+#define f_H 0x76
+#define f_h 0x74
+
+#define f_I 0x30
+#define f_i 0x10
+
+#define f_J 0x1E
+#define f_j 0x1E
+
+//#define f_K
+//#define f_k
+
+#define f_L 0x38
+#define f_l 0x30
+
+#define f_M 0x37
+#define f_m 0x37
+
+#define f_N 0x54
+#define f_n 0x54
+
+#define f_O 0x3F
+#define f_o 0x5C
+
+#define f_P 0x71
+#define f_p 0x71
+
+//#define f_Q
+//#define f_q
+
+#define f_R 0x50
+#define f_r 0x50
+
+#define f_S 0x6D
+#define f_s 0x6D
+
+#define f_T 0x70
+#define f_t 0x70
+
+#define f_U 0x3E
+#define f_u 0x1C
+
+#define f_V
+#define f_v
+
+//#define f_W
+//#define f_w
+
+//#define f_X
+//#define f_x
+
+#define f_Y 0x6E
+#define f_y 0X6E
+
+#define f_Z 0x9B
+#define f_z 0x9B
+
+#endif
