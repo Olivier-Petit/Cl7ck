c6d7e24bba4ec2f7dafbce3f2f30a041fe9b7b0f
op414 <lestyle414@hotmail.fr>
2013-06-22 13:45:01 +0200

    Buttons handling

---
+++ InputsHandler.cpp
@@ -0,0 +1,71 @@
+/*
+ * Cl7ck
+ * Author : op414
+ * http://op414.net
+ * License : CC BY-NC-SA http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * ------------
+ * Inputs Handler.cpp : Implements InputsHandler class : handles button presses
+ * Created 22 june 2013
+ */
+
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include <Arduino.h>
+#else
+  #include "WProgram.h"
+#endif
+
+#include "settings.h"
+#include "InputsHandler.h"
+
+InputsHandler::InputsHandler()
+{
+  // Initialize the buttons
+  for(int i = 0 ; i < 4 ; i++)
+  {
+    pinMode(m_buttonPins[i], OUTPUT);
+    m_lastDebounceTime[i] = 0UL;
+    m_buttonState[i] = LOW;
+    m_lastButtonState[i] = LOW;
+    m_buttonPulled[i] = false;
+  }
+}
+
+const int InputsHandler::m_buttonPins[4] = {PIN_BACK, PIN_MINUS, PIN_OK, PIN_PLUS};
+
+void InputsHandler::updateButtonStates()
+{
+  for(int i = 0 ; i < 4 ; i++)
+  {
+    boolean reading = digitalRead(m_buttonPins[i]);
+    
+    if(reading != m_lastButtonState[i])
+    {
+      m_lastDebounceTime[i] = millis();
+    }
+    else if(millis() - m_lastDebounceTime[i] >= DEBOUNCE_DELAY)
+    {
+      if(m_buttonState[i] == LOW)
+        m_buttonPulled[i] = false;
+      m_buttonState[i] = reading;
+    }
+      
+    m_lastButtonState[i] = reading;
+  }
+}
+
+boolean InputsHandler::pullButtonPress(int button)
+{
+  if(m_buttonState[button] == HIGH && m_buttonPulled[button] == false)
+  {
+    m_buttonPulled[button] = true;
+    return true;
+  }
+  
+  return false;
+}
+
+boolean InputsHandler::getButtonState(int button)
+{
+  return m_buttonState[button];
+}
+
---
+++ InputsHandler.h
@@ -0,0 +1,37 @@
+/*
+ * Cl7ck
+ * Author : op414
+ * http://op414.net
+ * License : CC BY-NC-SA http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * ------------
+ * Inputs Handler.h : InputsHandler class definition
+ * Created 22 june 2013
+ */
+
+
+#ifndef DEF_INPUTSHANDLER
+#define DEF_INPUTSHANDLER
+
+// Lambda enumeration for button names
+enum{BACK = 0, MINUS = 1, OK = 2, PLUS = 3};
+
+#define DEBOUNCE_DELAY 50UL
+
+class InputsHandler
+{
+  public:
+    InputsHandler();
+    void updateButtonStates();
+    boolean pullButtonPress(int button);
+    boolean getButtonState(int button);
+  
+  private:
+    unsigned long m_lastDebounceTime[4];
+    boolean m_buttonState[4];
+    boolean m_lastButtonState[4];
+    boolean m_buttonPulled[4];
+    static const int m_buttonPins[4];
+};
+
+#endif
+
